28f8f2349688f8c33be7ae87d424c86d1fe65453
juanlatorre <juanlatorreharcha@gmail.com>
2020-10-21 01:41:00 -0300

    UX/UI Card attempt 1

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,9 +1,18 @@
 import styled, { css } from "@emotion/native";
 import React, { FC } from "react";
 import { ImageBackground } from "react-native";
-import { Div, Icon, ScrollDiv, Text } from "react-native-magnus";
+import {
+  Button,
+  Div,
+  Icon,
+  Image,
+  ScrollDiv,
+  Tag,
+  Text,
+} from "react-native-magnus";
 import { theme } from "theme";
 import PatternHero from "@images/pattern_hero.png";
+import SelloCalidad from "@images/sello.png";
 
 const SafeArea = styled.SafeAreaView`
   flex: 1;
@@ -55,41 +64,105 @@ export const Home: FC = () => {
           mt={-80}
           mb={15}
           shadow="md"
-          flexDir="row"
+          flexDir="column"
+          alignItems="center"
           justifyContent="space-between"
         >
-          <Text>we</Text>
-          <Div flexDir="row">
-            <Icon
-              name="star"
-              color="yellow"
-              fontSize="title"
-              fontFamily="FontAwesome"
-            />
-            <Icon
-              name="star"
-              color="yellow"
-              fontSize="title"
-              fontFamily="FontAwesome"
-            />
-            <Icon
-              name="star"
-              color="yellow"
-              fontSize="title"
-              fontFamily="FontAwesome"
-            />
-            <Icon
-              name="star"
-              color="gray"
-              fontSize="title"
-              fontFamily="FontAwesome"
-            />
-            <Icon
-              name="star"
-              color="gray"
-              fontSize="title"
-              fontFamily="FontAwesome"
-            />
+          <Div
+            flexDir="row"
+            alignItems="center"
+            justifyContent="space-between"
+            px={5}
+            py={10}
+            w="100%"
+          >
+            <Text fontSize="lg">Álvaro Villanueva</Text>
+            <Div flexDir="row">
+              <Icon
+                name="star"
+                color="yellow"
+                fontSize="title"
+                fontFamily="FontAwesome"
+              />
+              <Icon
+                name="star"
+                color="yellow"
+                fontSize="title"
+                fontFamily="FontAwesome"
+              />
+              <Icon
+                name="star"
+                color="yellow"
+                fontSize="title"
+                fontFamily="FontAwesome"
+              />
+              <Icon
+                name="star"
+                color="gray"
+                fontSize="title"
+                fontFamily="FontAwesome"
+              />
+              <Icon
+                name="star"
+                color="gray"
+                fontSize="title"
+                fontFamily="FontAwesome"
+              />
+            </Div>
+          </Div>
+
+          <Div
+            flexDir="row"
+            alignItems="center"
+            justifyContent="space-between"
+            px={5}
+            py={10}
+            w="100%"
+          >
+            <Div flexDir="row" alignItems="center">
+              <Tag rounded="2xl" bg="red" color="white" fontSize="xs">
+                Saco
+              </Tag>
+              <Tag rounded="2xl" ml="md" bg="red" color="white" fontSize="xs">
+                Metro
+              </Tag>
+              <Tag rounded="2xl" ml="md" bg="red" color="white" fontSize="xs">
+                Cortado
+              </Tag>
+            </Div>
+
+            <Div flexDir="row" alignItems="center"></Div>
+              <Tag rounded="2xl" ml="md" bg="yellow" fontSize="xs">
+                Nativa
+              </Tag>
+              <Tag rounded="2xl" ml="md" bg="yellow" fontSize="xs">
+                Eucaliptus
+              </Tag>
+            </Div>
+          </Div>
+
+          <Div
+            flexDir="row"
+            alignItems="center"
+            justifyContent="space-between"
+            px={5}
+            py={10}
+            w="100%"
+          >
+            <Div flexDir="row" alignItems="center">
+              <Image h={18} w={18} source={SelloCalidad} />
+              <Text ml="md">Certificada</Text>
+            </Div>
+            <Button
+              px="lg"
+              py="md"
+              color="white"
+              bg="blue"
+              rounded="2xl"
+              suffix={<Icon ml="md" name="arrowright" color="white" />}
+            >
+              Ver más
+            </Button>
           </Div>
         </Div>
 
